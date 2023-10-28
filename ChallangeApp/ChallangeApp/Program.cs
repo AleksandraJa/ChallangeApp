55ed603bdb1b7d422222942bb7af9bd90786ece6
Ale Jaga <aleksandra.jagiello@outlook.com>
2023-10-28 16:36:26 +0200

    Zadanie z dnia 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var name = "Olek";
+var age = 30;
+var sex = "maly chlopiec";
+
+if (name == "Olek" && age < 50)
+{
+    Console.WriteLine("MAM MNIEJ NIZ = 35 LAT");
+}
+else if (age != 30 && name == "Olek") ;
+{
+    Console.WriteLine("JESTEM OLKIEM PRZED 50");
+}
+if (sex == "maly chlopiec" && age == 35) ;
+{
+    Console.WriteLine("JESTEM MALYM CHLOPCEM");
+}
+if (sex == "maly chlopiec") ;
+{
+    Console.WriteLine("JESTEM KIMS INNYM");
+    Console.WriteLine("JESTEM KIMS INNYM");
+    Console.WriteLine("JESTEM KIMS INNYM");
+}
